cfd47d2320f7d513b7dc67c5db33d474484ba45e
karlijnd <karlijn_dinnissen@hotmail.com>
2014-12-21 22:31:56 +0100

    Finalized comments of Progamming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,52 @@
 ## These functions cache the inverse of a matrix
 ## When a requested matrix is already cached, it saves time and computation power
 
-## create a special "matrix" object that can cache its inverse
+## Create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-    ## set the value of the inverted matrix
+
     s <- NULL
+    
+    ## setter
     set <- function(y) {
         x <<- y
         s <<- NULL
     }
-    ## get the values of the matrix
+    
+    ## getter
     get <- function() x
-    ## invert the matrix
+    
+    ## Set inverted matrix.
     setsolve <- function(solve) s <<- solve
-    ## get the inverted matrix
+    
+    ## Get the inverted matrix.
     getsolve <- function() s
+    
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
 }
 
-
-## compute the inverse of the special "matrix" returned by makeCacheMatrix above.
-## If the inverse has already been calculated (and the matrix has not changed)
+## Compute the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
 ## then the cachesolve should retrieve the inverse from the cache.
-
 cacheSolve <- function(x, ...) {
-    ## nog niet aangepast
-    ## calculates the mean of the special "vector" created with the above function.
-     
+    
+    ## Checks to see if the inverse has already been calculated.
     s <- x$getsolve()
-    ## checks to see if the mean has already been calculated.
+
+    ## If so, it gets the inverse from the cache and skips the computation.
     if(!is.null(s)) {
         message("getting cached data")
-        ## If so, it gets the mean from the cache and skips the computation.
         return(s)
     }
     data <- x$get()
-    ## Otherwise, it calculates the mean of the data
-    s <- solve(data, ...)
-    ## and sets the value of the mean in the cache via the setmean function.
-    x$setsolve(s)
-    s
     
-    ## Return a matrix that is the inverse of 'x'
+    ## Otherwise, it calculates the inverse of the matrix...
+    s <- solve(data, ...)
+    
+    ## and sets the value of the inverse in the cache via the setsolve function.
+    x$setsolve(s)
+    
+    ## Return a matrix that is the inverse of 'x'.
+    s
 }
